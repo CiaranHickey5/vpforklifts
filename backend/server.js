a0d0a14121c219a14a5bd518f851442f275b5cbb
Ciaran Hickey <ciaran.hickey99@gmail.com>
2025-06-30 13:06:29 +0100

    Add path import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require('path');
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 const compression = require("compression");
